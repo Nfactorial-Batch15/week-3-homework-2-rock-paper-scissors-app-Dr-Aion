56800c3cd5506aa1fe639d821562bdaf11440df3
Dr-Aion <aiganym.moldagulova@gmail.com>
2021-12-16 11:11:00 +0400

    Initial Commit

---
+++ HW8/HW8App.swift
@@ -0,0 +1,17 @@
+//
+//  HW8App.swift
+//  HW8
+//
+//  Created by Aiganym Moldagulova on 16/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct HW8App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
